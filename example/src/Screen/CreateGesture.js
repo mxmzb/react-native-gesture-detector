cd4010f39c95f9a5f1b90e6fb7664bca0d790030
Maxim Zubarev <maxim.zubarev.dev@icloud.com>
2019-09-14 13:43:54 +0200

    example screen refinements

--- example/src/Screen/CreateGesture.js
+++ example/src/Screen/CreateGesture.js
@@ -7,80 +7,84 @@ import GestureDetector, {
 } from "react-native-gesture-detector";
 
 const CreateGestureScreen = () => {
+  const [tmpOffset, setTmpOffset] = useState(null);
+  const [finishedGesture, setFinishedGesture] = useState([]);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <GestureRecorder>
-        {({ gesture }) => {
-          console.log("gesture", gesture);
+      <GestureRecorder onPanRelease={gesture => setFinishedGesture(gesture)}>
+        {({ gesture, offset }) => {
+          if (tmpOffset !== offset && offset !== null) {
+            setTmpOffset(offset);
+          }
+
           return (
             <View
               style={{
                 position: "relative",
                 width: "90%",
-                height: "100%",
+                height: "50%",
                 left: "5%",
                 backgroundColor: "#eee",
               }}
             >
               <View style={{ position: "relative", width: "100%", height: "100%" }}>
-                <GesturePath path={gesture} color="green" slopRadius={30} />
+                <GesturePath
+                  path={gesture.map(coordinate => {
+                    if (!tmpOffset) {
+                      return coordinate;
+                    }
+
+                    return {
+                      x: coordinate.x + tmpOffset.x,
+                      y: coordinate.y + tmpOffset.y,
+                    };
+                  })}
+                  color="green"
+                  slopRadius={30}
+                  center={false}
+                />
               </View>
             </View>
           );
         }}
       </GestureRecorder>
-      {/* <View style={{ flex: 1 }}>
-        <View
-          style={{
-            height: "90%",
-            top: "5%",
-          }}
-        >
-          <GestureDetector
-            onGestureFinish={gesture => Alert.alert(`Gesture ${gesture} finished!`)}
-            onProgress={({ gesture, progress }) => {
-              setProgress(progress);
-              setGesture(gesture);
-            }}
-            onPanRelease={() => {
-              setProgress(null);
-              setGesture(null);
-            }}
-            gestures={gestures}
-          >
-            {({ coordinate }) => (
-              <View
-                style={{
-                  position: "relative",
-                  width: "90%",
-                  height: "100%",
-                  left: "5%",
-                  backgroundColor: "#eee",
-                }}
-              >
-                <View style={{ position: "relative", width: "100%", height: "100%" }}>
-                  <GesturePath path={gestures["Triangle"]} color="green" slopRadius={30} />
-                </View>
-                {coordinate && <Cursor {...coordinate} />}
-              </View>
-            )}
-          </GestureDetector>
-        </View>
 
-        <View
-          style={{
-            backgroundColor: "red",
-            width: "90%",
-            minHeight: 50,
-            left: "5%",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Text>Gesture: {gesture}</Text>
-          <Text>Progress: {progress}</Text>
-        </View>
-      </View> */}
+      <GestureDetector
+        onGestureFinish={() => Alert.alert(`Custom User Gesture finished!`)}
+        gestures={{ CustomUserGesture: finishedGesture }}
+      >
+        {({ coordinate }) => (
+          <View
+            style={{
+              position: "relative",
+              width: "90%",
+              height: "50%",
+              left: "5%",
+              backgroundColor: "red",
+            }}
+          >
+            <View style={{ position: "relative", width: "100%", height: "100%" }}>
+              <GesturePath
+                path={finishedGesture.map(coordinate => {
+                  if (!tmpOffset) {
+                    return coordinate;
+                  }
+
+                  return {
+                    x: coordinate.x + tmpOffset.x,
+                    y: coordinate.y + tmpOffset.y,
+                  };
+                })}
+                color="green"
+                slopRadius={30}
+                center={false}
+              />
+            </View>
+            {coordinate && <Cursor {...coordinate} />}
+          </View>
+        )}
+      </GestureDetector>
     </SafeAreaView>
   );
 };
